2bf3a533f32b280a8bf230bf358f55d01ef762b2
Luiz Eduardo Satelis dos Santos <edusatelis@gmail.com>
2021-04-24 17:16:29 -0300

    Adding new modules and responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule

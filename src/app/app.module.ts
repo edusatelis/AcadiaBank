dfe7b0ea090d32f59414cdd7dccace0af4947fca
Luiz Eduardo Satelis dos Santos <edusatelis@gmail.com>
2021-04-25 13:21:51 -0300

    Changing structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { ServicesComponent } from './services/services.component';
-import { CreditCardComponent } from './credit-card/credit-card.component';
-import { InsuranceComponent } from './insurance/insurance.component';
-import { MobileComponent } from './mobile/mobile.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { InsuranceComponent } from './components/insurance/insurance.component';
+import { MobileComponent } from './components/mobile/mobile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
